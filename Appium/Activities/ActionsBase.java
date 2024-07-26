61fa64c07fea28d1294df209dd3d3e4ca09e8c40
supraja-code <supraja.bhargavi.m@ibm.com>
2024-07-26 18:01:11 +0530

    Add files via upload

---
+++ Appium/Activities/ActionsBase.java
@@ -0,0 +1,31 @@
+package activities;
+
+import java.util.Arrays;
+
+import org.openqa.selenium.Point;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+
+import io.appium.java_client.AppiumDriver;
+
+import static java.time.Duration.ofMillis;
+import static org.openqa.selenium.interactions.PointerInput.MouseButton.LEFT;
+import static org.openqa.selenium.interactions.PointerInput.Origin.viewport;
+
+public class ActionsBase {
+	//Create the pointer
+	private static final PointerInput pointer = new PointerInput(PointerInput.Kind.TOUCH,"finger");
+	
+	//Create the action
+	public static void doSwipe(AppiumDriver driver, Point start, Point end, int duration) {
+		//Create the sequence of actions
+		Sequence swipe = new Sequence(pointer,1)
+				.addAction(pointer.createPointerMove(ofMillis(0), viewport(), start.getX(), start.getY()))
+				.addAction(pointer.createPointerDown(LEFT.asArg()))
+				.addAction(pointer.createPointerMove(ofMillis(duration), viewport(), end.getX(), end.getY()))
+				.addAction(pointer.createPointerUp(LEFT.asArg()));
+		
+		//Perform the actions
+		driver.perform(Arrays.asList(swipe));
+	}
+}
